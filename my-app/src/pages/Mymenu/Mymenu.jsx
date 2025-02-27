dcbf01f5538c05a4f476e3c9dcde5c0894843333
leegain1 <gainlee@kookmin.ac.kr>
2025-02-27 14:01:40 +0900

    수정수정

--- my-app/src/pages/Mymenu/Mymenu.jsx
+++ my-app/src/pages/Mymenu/Mymenu.jsx
@@ -10,7 +10,8 @@ import trash from "./trash.svg";
 export const Mymenu = () => {
     return (
         <div className="screen">
-            <div className="div">
+            {/*id="elementA" style="color: blue;"*/}
+            <div>
                 <div className="overlap">
                     <img className="profile" alt="Profile" src={profile} />
                     <img className="pen" alt="Pen" src={pen} />
@@ -24,34 +25,37 @@ export const Mymenu = () => {
 
                 <div className="overlap-group">
                     <div className="frame">
-                        <div className="rectangle" />
+                        <div className="rectangle">
+                            <div className="frame-2">
+                                <div className="overlap-group-wrapper">
+                                    <div className="overlap-2">
+                                        <div className="text-wrapper-2">불고기</div>
+
+                                        <img className="trash" alt="Trash" src={trash} />
+                                    </div>
+                                </div>
+
+                                <div className="overlap-group-wrapper">
+                                    <div className="overlap-2">
+                                        <div className="text-wrapper-2">김치찌개</div>
+
+                                        <img className="trash" alt="Trash" src={trash} />
+                                    </div>
+                                </div>
+
+                                <div className="overlap-group-wrapper">
+                                    <div className="overlap-2">
+                                        <div className="text-wrapper-2">제육볶음</div>
+
+                                        <img className="trash" alt="Trash" src={trash} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
-                    <div className="frame-2">
-                        <div className="overlap-group-wrapper">
-                            <div className="overlap-2">
-                                <div className="text-wrapper-2">불고기</div>
 
-                                <img className="trash" alt="Trash" src={trash} />
-                            </div>
-                        </div>
-
-                        <div className="overlap-group-wrapper">
-                            <div className="overlap-2">
-                                <div className="text-wrapper-2">김치찌개</div>
-
-                                <img className="trash" alt="Trash" src={trash} />
-                            </div>
-                        </div>
-
-                        <div className="overlap-group-wrapper">
-                            <div className="overlap-2">
-                                <div className="text-wrapper-2">제육볶음</div>
-
-                                <img className="trash" alt="Trash" src={trash} />
-                            </div>
-                        </div>
-                    </div>
                 </div>
 
 
