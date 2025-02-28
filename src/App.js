6c9185c5322ba2eab10d338f54931e0d62a2a52e
강보경 <bbibbi@gangbogyeong-ui-noteubug.local>
2025-03-01 01:12:27 +0900

    홈페이지 링크 수정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Category from "./pages/Category/Category";
 import CategoryDetail from "./pages/Category/CategoryDetail";
 import Mymenu from "./pages/Mymenu/Mymenu";  // Mymenu 페이지 import
-import Home from "./pages/Home";
+import Home from "./pages/Roulette/roulette";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/Roulette" element={<Home />} />
             <Route path="/category" element={<Category />} />
             <Route path="/category/:categoryName" element={<CategoryDetail />} />
             <Route path="/Mymenu" element={<Mymenu />} />  {/* /Mymenu 경로 추가 */}
