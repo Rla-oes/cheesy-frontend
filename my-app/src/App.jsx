3de009cb9b69f4ca742c2ea2974be757d9f542de
leegain1 <gainlee@kookmin.ac.kr>
2025-02-28 22:55:18 +0900

    jsx파일 js로 변경 후 하나의 src에 병합, mymenu페이지 수정

--- my-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-import './App.css'
-import Router from "./routes/Router.jsx"
-
-function App() {
-    return <Router />;
-}
-
-export default App
--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Category from "./pages/Category";
-import CategoryDetail from "./pages/CategoryDetail"; // 새로운 상세 페이지 추가
+import Category from "./pages/Category/Category";
+import CategoryDetail from "./pages/Category/CategoryDetail"; // 새로운 상세 페이지 추가
+import Mymenu from "./pages/Mymenu/Mymenu";  // Mymenu 페이지 import
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Category />} />
-      <Route path="/category/:categoryName" element={<CategoryDetail />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Category />} />
+            <Route path="/category/:categoryName" element={<CategoryDetail />} />
+            <Route path="/Mymenu" element={<Mymenu />} />  {/* /Mymenu 경로 추가 */}
+        </Routes>
+    );
 }
 
 export default App;
