553014e798eac1496774a7e6167d15adebea404d
dlwhddbs0114 <ljy050114@naver.com>
2025-03-05 15:52:47 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Category from "./pages/Category/Category";
-import CategoryDetail from "./pages/Category/CategoryDetail";
 import Mymenu from "./pages/Mymenu/Mymenu";  // Mymenu 페이지 import
 import Result from "./pages/Result/Result"; // Result 페이지 import
 
@@ -15,7 +14,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/Roulette" element={<Roulette />} />
             <Route path="/category" element={<Category />} />
-            <Route path="/category/:categoryName" element={<CategoryDetail />} />
             <Route path="/Mymenu" element={<Mymenu />} />  {/* /Mymenu 경로 추가 */}
             <Route path={"/Result"} element={<Result />} /> {/* /Result 경로 추가 */}
         </Routes>
