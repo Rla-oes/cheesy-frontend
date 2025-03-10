5cfa111b6142052d292775da9a2dbdda08914737
leegain1 <gainlee@kookmin.ac.kr>
2025-03-10 17:16:23 +0900

    Start페이지 뒤에 Home페이지 2초뒤 이동

--- src/pages/Start/Start.js
+++ src/pages/Start/Start.js
@@ -1,9 +1,19 @@
-import React, { useState} from "react";
+import React, { useEffect, useState } from "react"; // useEffect 추가
 import { useNavigate } from "react-router-dom";
 import "./Start.css";
 
 export const Start = () => {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        // 2초 후에 Home 페이지로 이동
+        const timer = setTimeout(() => {
+            navigate("/Home");
+        }, 2000);
+
+        // 컴포넌트가 언마운트될 때 타이머 클리어
+        return () => clearTimeout(timer);
+    }, [navigate]);
 
     return (
         <div className="screen">
@@ -16,14 +26,10 @@ export const Start = () => {
 
                     <div className="title-text-wrapper">formymeal_</div>
 
-
                 </div>
             </div>
         </div>
-        );
-    };
+    );
+};
 
-
-export default Start
-
-
+export default Start;
